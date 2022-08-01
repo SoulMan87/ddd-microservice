3f43e0499546e54fda40de0697a40d917ebe5992
Jonathan Hinestroza <jonathanhinestroza87@gmail.com>
2022-08-01 17:28:03 -0500

    Dtos

---
+++ src/main/java/com/soultech/ddd/microservice/dto/CreateCourseResponse.java
@@ -0,0 +1,16 @@
+package com.soultech.ddd.microservice.dto;
+
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class CreateCourseResponse {
+
+    Boolean existsAlready;
+
+    Integer courseId;
+}
